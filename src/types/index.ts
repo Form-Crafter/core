c70fc2167bcc424a950ae8a3d7eaa9fa96c37541
Лазука Егор Александрович <ELazuka_in@inno.local>
2025-01-03 19:40:16 +0300

    added types, fix package.json

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,21 @@
+type OptionalKeys<T extends Object> = keyof {
+    [K in keyof T as undefined extends T[K] ? K : never]: T[K]
+};
+
+type RequiredKeys<T extends Object> = keyof Omit<T, OptionalKeys<T>>;
+
+type PartialIfUndefined<T extends Object> =
+    {
+        [K in keyof T as K extends OptionalKeys<T> ? K : never]?: T[K]
+    } &
+    {
+        [K in keyof T as K extends RequiredKeys<T> ? K : never]: T[K]
+    };
+
+export type GetComponentPropertiesSchema<T extends Record<string, {__outputType: any}>> = PartialIfUndefined<{
+    [K in keyof T]: T[K]['__outputType']
+}>;
+
 export type SelectionOption = {
     label: string;
     value: string;
