614c50a561fd77214e7aa3ff75a91047dfd8b9fd
Лазука Егор Александрович <ELazuka_in@inno.local>
2025-02-15 23:49:19 +0300

    fix

--- src/utils/get-component-depth.ts
+++ src/utils/get-component-depth.ts
@@ -3,8 +3,17 @@ import { isEmpty } from '@form-crafter/utils'
 import { EntityId, ViewDefinitionComponents } from '_types'
 
 export const getComponentDepth = (id: EntityId, componentsMap: ViewDefinitionComponents): number => {
+    console.log('id: ', id)
+    console.log('componentsMap: ', componentsMap)
+
     const execute = (currentId: EntityId, depth: number): number => {
+        console.log('currentId: ', currentId)
+        console.log('depth: ', depth)
+
         const { parentId } = componentsMap[currentId]
+        console.log('parentId: ', parentId)
+        console.log('-: '.repeat(100))
+
         if (isEmpty(parentId)) {
             return depth
         }
