dad3be92d8734bac2ae9ab610d4241832b473bed
Лазука Егор Александрович <ELazuka_in@inno.local>
2025-02-16 00:48:52 +0300

    fix

--- src/utils/get-component-depth.ts
+++ src/utils/get-component-depth.ts
@@ -5,15 +5,8 @@ import { EntityId, ViewDefinitionComponents } from '_types'
 const deepLevelLeft = 2
 
 export const getComponentDepth = (id: EntityId, componentsMap: ViewDefinitionComponents): number => {
-    console.log('FUCK!!!!!!!!!!!!!!!!!!!!!!!!!!')
-
     const execute = (currentId: EntityId, depth: number): number => {
-        console.log('currentId: ', currentId)
-        console.log('depth: ', depth)
-
         const { parentId } = componentsMap[currentId]
-        console.log('parentId: ', parentId)
-        console.log('-: '.repeat(100))
 
         if (isEmpty(parentId)) {
             return depth
